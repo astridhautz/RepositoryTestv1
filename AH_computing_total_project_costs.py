c4d023133e6790417a0719dd8b3cb8833ea4050b
Astrid763 <56829393+Astrid763@users.noreply.github.com>
2021-01-04 16:00:30 +0100

    read CSV
    read csv

---
+++ AH_computing_total_project_costs.py
@@ -0,0 +1,9 @@
+#%%
+import pandas as pd 
+import numpy as np
+
+#%%
+df = pd.read_csv(r'C:\Users\Astrid.Hautz\EUROVIA\WeAreLeonardo_AI Innovation - General\Leonard_DACH_2020\Data\280_Bottrop\280_Bottrop\1039_1187.csv')
+print(df)
+df.head()
+# %%
